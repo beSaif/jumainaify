9793ed53bdef1aeeed950a9714d9aa81d6fde0b8
SAIF H K M <hishamkolikara@gmail.com>
2022-04-01 06:57:27 +0530

    Added: last cried

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:jumainaify/sizedbox.dart';
+import 'package:intl/intl.dart';
 
 const colorAccent = Color.fromRGBO(178, 190, 219, 1);
 
@@ -13,7 +14,19 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  late int count;
+  late int count = 2;
+  late Timestamp lastcried;
+  updatedata(int count) {
+    Map<String, dynamic> data = {
+      'count': count,
+      'time': FieldValue.serverTimestamp()
+    };
+    FirebaseFirestore.instance
+        .collection("collection")
+        .doc('counter')
+        .update(data);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,83 +37,94 @@ class _HomeState extends State<Home> {
           image: AssetImage("images/bg.jpg"),
           fit: BoxFit.cover,
         )),
-        child: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text("Jumainaify",
-                style: TextStyle(
-                    fontFamily: 'Albegos', color: Colors.white, fontSize: 45)),
-            Text("+1 everytime Jum cries for no reason",
-                style: GoogleFonts.inter(
-                    color: Colors.white60, fontWeight: FontWeight.w600)),
-            verticalBox(30),
-            Row(
+        child: StreamBuilder(
+          stream:
+              FirebaseFirestore.instance.collection('collection').snapshots(),
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            DocumentSnapshot ds = snapshot.data!.docs[0];
+            String formattedDate = ds['time'] == null
+                ? ""
+                : DateFormat('dd-MM (kk:mm)').format(ds['time'].toDate());
+            return Center(
+                child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                GestureDetector(
-                  onTap: () {
-                    if (count != 0) {
-                      //print("minus pressed");
-                      count--;
-                      FirebaseFirestore.instance
-                          .collection("collection")
-                          .doc('counter')
-                          .update({'count': count});
-                    }
-                  },
-                  child: const Text("-",
-                      style: TextStyle(
-                          fontFamily: 'Albegos',
-                          color: Colors.white,
-                          fontSize: 40)),
-                ),
-                horizontalBox(30),
-                StreamBuilder(
-                  stream: FirebaseFirestore.instance
-                      .collection('collection')
-                      .snapshots(),
-                  builder: (BuildContext context,
-                      AsyncSnapshot<QuerySnapshot> snapshot) {
-                    if (!snapshot.hasData) {
-                      return const Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    }
-
-                    return Row(
-                      children: snapshot.data!.docs.map((document) {
-                        count = document['count'];
-                        return Text(document['count'].toString(),
+                const Text("Jumainaify",
+                    style: TextStyle(
+                        fontFamily: 'Albegos',
+                        color: Colors.white,
+                        fontSize: 45)),
+                Text("+1 everytime Jum cries for no reason",
+                    style: GoogleFonts.inter(
+                        color: Colors.white60, fontWeight: FontWeight.w600)),
+                verticalBox(30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        if (count != 0) {
+                          //print("minus pressed");
+                          count--;
+                          updatedata(count);
+                        }
+                      },
+                      child: const Text("-",
+                          style: TextStyle(
+                              fontFamily: 'Albegos',
+                              color: Colors.white,
+                              fontSize: 40)),
+                    ),
+                    horizontalBox(30),
+                    Row(
+                      children: [
+                        Text(ds['count'].toString(),
                             style: const TextStyle(
                                 fontFamily: 'Albegos',
                                 color: Colors.white,
-                                fontSize: 70));
-                      }).toList(),
-                    );
-                  },
+                                fontSize: 70)),
+                      ],
+                    ),
+                    horizontalBox(30),
+                    GestureDetector(
+                      onTap: () {
+                        //print("plus pressed");
+                        count++;
+                        updatedata(count);
+                      },
+                      child: const Text("+",
+                          style: TextStyle(
+                              fontFamily: 'Albegos',
+                              color: Colors.white,
+                              fontSize: 40)),
+                    ),
+                  ],
                 ),
-                horizontalBox(30),
-                GestureDetector(
-                  onTap: () {
-                    //print("plus pressed");
-                    count++;
-                    FirebaseFirestore.instance
-                        .collection("collection")
-                        .doc('counter')
-                        .update({'count': count});
-                  },
-                  child: const Text("+",
-                      style: TextStyle(
-                          fontFamily: 'Albegos',
-                          color: Colors.white,
-                          fontSize: 40)),
+                verticalBox(10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("last cried on: ",
+                        style: GoogleFonts.inter(
+                            color: Colors.white60,
+                            fontWeight: FontWeight.w300)),
+                    Text(formattedDate,
+                        style: GoogleFonts.inter(
+                            color: Colors.white60,
+                            fontWeight: FontWeight.w400)),
+                  ],
                 ),
               ],
-            )
-          ],
-        )),
+            ));
+          },
+        ),
       ),
     );
   }
